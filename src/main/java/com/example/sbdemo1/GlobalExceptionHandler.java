cfc758e07ee4f8da039c6a884a6103e92d454a05
czy <541644279@qq.com>
2021-02-08 10:36:33 +0800

    hotfix

--- src/main/java/com/example/sbdemo1/GlobalExceptionHandler.java
+++ src/main/java/com/example/sbdemo1/GlobalExceptionHandler.java
@@ -26,7 +26,7 @@ public class GlobalExceptionHandler {
         BindingResult bindingResult = e.getBindingResult();
         System.out.println(Size.class.getName());
         System.out.println(bindingResult.getFieldError().getCode());
-        System.out.println(bindingResult.getFieldError().getField());
+        System.out.println(bindingResult.getFieldError().getField());//Field还在
         System.out.println(bindingResult.getFieldError().getDefaultMessage());
         Ajaxresult ajaxresult = new Ajaxresult(null,false);
         ajaxresult.setSuccess(false);
