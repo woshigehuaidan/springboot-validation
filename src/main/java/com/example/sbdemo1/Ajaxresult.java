e5090086a562cacea72ad50bdf0fa130e5ef8738
czy <541644279@qq.com>
2021-02-16 08:43:13 +0800

    Git是否产生了冲突

--- src/main/java/com/example/sbdemo1/Ajaxresult.java
+++ src/main/java/com/example/sbdemo1/Ajaxresult.java
@@ -37,7 +37,7 @@ public class Ajaxresult {
     public void setResultInfo(String resultInfo) {
         this.resultInfo = resultInfo;
     }
-    //Git产生冲突了吧?
+    //Git是否产生冲突了吗?
     public boolean isSuccess() {
         return success;
     }
